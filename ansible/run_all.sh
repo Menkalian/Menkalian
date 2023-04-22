1e8be9d73bc29ed9e43528fef73c2d0553e924e5
Kilian Krampf <kilian@krampf.de>
2023-04-22 13:46:35 +0200

    Add automatic requirement handling

--- ansible/run_all.sh
+++ ansible/run_all.sh
@@ -5,4 +5,5 @@ if [[ -f ~/.ansible.cfg ]]; then
     cp ~/.ansible.cfg ~/.ansible.cfg.bak
 fi
 cp ansible.cfg ~/.ansible.cfg
+ansible-galaxy install -r requirements.yml
 ansible-playbook --vault-password-file=.vaultpass -i inventory.yml site.yml
